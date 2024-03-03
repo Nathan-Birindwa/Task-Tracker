4b5b9462f864ddf519375104b5e31ef6c47fe722
Nathan-Birindwa <birhindwan@gmail.com>
2024-03-03 15:18:02 +0300

    Finished the main page

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # path("<str:task_id>", )
+    path("add", views.add, name="add"),
 ]
 
--- tasks/views.py
+++ tasks/views.py
@@ -5,17 +5,29 @@ from django import forms
 class add_Item_Form(forms.Form):
     task = forms.CharField(label="Add Task", max_length=100)
 
-List = ["Home", "School"]
+List = [{
+    "id": 1,
+    "name": "Go to gym",
+    "dec": "Go and burn out some fats to not end like you know"},
+    {
+    "id": 1,
+    "name": "Go to Town",
+    "dec": "Go and burn out some fats to not end like you know"},
+    {
+    "id": 1,
+    "name": "Go to Town",
+    "dec": "Go and burn out some fats to not end like you know"},
+
+        ]
 
 # Create your views here.
 def index(request):
-    if request.method == "post":
-        form = add_Item_Form(request.POST)
-        if form.is_valid():
-            form.cleaned_data["task"]
+   
     
     return render(request, 'main/index.html', {
         "tasks": Task.objects.all(),
         "Input": add_Item_Form(),
         "List": List
     })
+def add(request):
+    return render(request, 'Task/add.html')
